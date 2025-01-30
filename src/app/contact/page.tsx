e8fca6120c6c68cb518c189c017321d96bb44cb9
FransAr16 <fransaryadita@gmail.com>
2025-01-30 12:13:44 +0700

    f675876

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,14 +6,14 @@ import Headers from "@/components/ui/Headers";
 import TextSlideUp from "@/components/ui/TextSlideUp";
 import { revealsText } from "@/data/animation";
 
-export default function Playground() {
+export default function Contact() {
   return (
     <MainLayout>
       <Headers />
       <main className="relative overflow-hidden w-full h-full">
         <div className="w-full h-full relative">
           <div className="flex h-screen w-full justify-center items-center lg:text-[6rem] 2xl:text-[8rem] font-bold">
-            <TextSlideUp text="Playground" animate={revealsText} className="" />
+            <TextSlideUp text="Contact" animate={revealsText} className="" />
           </div>
         </div>
       </main>
