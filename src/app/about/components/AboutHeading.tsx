5dc86b6bd357e22d565aee24166077b3567a58b4
FransAr16 <fransaryadita@gmail.com>
2025-02-01 21:11:48 +0700

    qqwwqw

--- src/app/about/components/AboutHeading.tsx
+++ src/app/about/components/AboutHeading.tsx
@@ -12,8 +12,8 @@ export default function AboutHeading() {
   const isInViewImg = useInView(ref, { once: true });
 
   return (
-    <div className="relative h-full w-full py-[3rem] lg:py-[10rem] 2xl:py-[17rem] overflow-hidden">
-      <div className="flex flex-col lg:flex-row pt-[6rem] sm:pt-[10rem] lg:pt-[2.5rem] justify-between w-full gap-6 2xl:gap-[8rem] lg:gap-[1.5rem]">
+    <div className="relative w-full">
+      <div className="flex flex-col lg:flex-row justify-between w-full gap-6 2xl:gap-[8rem] lg:gap-[1.5rem]">
         <div className="h-full w-full lg:w-[71%] flex flex-col lg:items-end gap-[1rem]">
           <div ref={ref}>
             <motion.div
@@ -29,7 +29,7 @@ export default function AboutHeading() {
                 clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
               }}
               transition={{ delay: 0.5, duration: 1, ease: "easeInOut" }}
-              className="w-[60%] h-full sm:w-[38%] md:w-[32%] lg:w-[15rem] xl:w-[20vw] 2xl:w-[26rem] lg:pt-[1rem]"
+              className="w-[60%] h-full sm:w-[38%] md:w-[32%] lg:w-[15rem] xl:w-[20vw] 2xl:w-[25rem] lg:pt-[1rem]"
             >
               <motion.div
                 initial={{ scale: 1.5 }}
@@ -48,7 +48,7 @@ export default function AboutHeading() {
             </motion.div>
           </div>
 
-          <div className="w-[70%] sm:w-[14rem] lg:w-[15rem] xl:w-[20vw] 2xl:w-[26rem] order-last">
+          <div className="w-[70%] sm:w-[14rem] lg:w-[15rem] xl:w-[20vw] 2xl:w-[25rem] order-last">
             <TextSlideUp
               text="© 2024 Frans - Frontend Developer"
               animate={revealsText}
@@ -65,7 +65,7 @@ export default function AboutHeading() {
                 <TextSlideUp
                   text="I blend design and development to deliver impactful experiences."
                   animate={revealsText}
-                  className="font-medium mr-[8px] xl:mr-[18px] 2xl:mr-[20px] text-[clamp(2.3rem,10vw,10rem)] leading-[110%] sm:text-[5.5vw] sm:leading-[110%] lg:text-[5.2vw] lg:leading-[106%]"
+                  className="font-medium mr-[8px] xl:mr-[18px] 2xl:mr-[20px] text-[clamp(2.3rem,10vw,10rem)] sm:text-[5.5vw] 2xl:text-[5.2vw] leading-[106%]"
                 />
               </div>
 
--- src/app/about/components/AboutHero.tsx
+++ src/app/about/components/AboutHero.tsx
@@ -3,8 +3,8 @@ import AboutHeading from "./AboutHeading";
 
 export default function AboutHero() {
   return (
-    <div className="h-full lg:h-screen w-full relative overflow-hidden">
-      <div className="main-container">
+    <div className="h-[116vh] lg:h-[94vh] w-full relative overflow-hidden">
+      <div className="main-container w-full h-full flex items-end">
           <AboutHeading />
       </div>
     </div>
