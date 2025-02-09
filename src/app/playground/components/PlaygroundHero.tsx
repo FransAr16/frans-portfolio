e3994db4e8e3278e8eda8d12cc70da3795940abe
FransAr16 <fransaryadita@gmail.com>
2025-02-10 00:59:52 +0700

    dd

--- src/app/playground/components/PlaygroundHero.tsx
+++ src/app/playground/components/PlaygroundHero.tsx
@@ -6,6 +6,7 @@ export default function PlaygroundHero() {
     <div className="w-full h-full relative overflow-hidden">
       <div className="main-container pt-[8rem]">
         <h1 className="text-3xl font-bold mb-6">My Projects</h1>
+        <Projects />
       </div>
     </div>
   );
